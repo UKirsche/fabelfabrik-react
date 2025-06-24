3bf4feace3815d4c0d2f66c9da690c3b492fa7ed
Uwe Kirschenmann <ukirschenmann@gmx.de>
2025-06-24 13:51:54 +0200

    Falsche Route korrigiert - und fallback rein

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
-// export const API_BASE_URL = 'https://fabelfabrik.onrender.com/api';
-export const API_BASE_URL = 'http://192.168.179.51:8080/api';
+export const API_BASE_URL = 'https://fabelfabrik.onrender.com/api';
+// export const API_BASE_URL = 'http://192.168.179.51:8080/api';
 // Endpunkte für Medien
 export const IMAGES_BASE_URL = `${API_BASE_URL}/image/`;
 export const AUDIO_BASE_URL = `${API_BASE_URL}/audio/`;
